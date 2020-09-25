9b22408990c2ab26f9168f546071adde8055b79e
Allen <l4IQ3GAOQzs8968U>
2020-09-25 15:35:28 +0800

    debug

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,6 +1,6 @@
 import _Vue from "vue";
 export * from "./lib/utils";
-export * from "./lib/utils";
+export * from "./lib/url";
 export * from "./lib/cookie";
 export * from "./lib/float";
 declare const _default: (Vue: typeof _Vue) => void;
